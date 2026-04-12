2d9d2c7510935ccb5950b5c49ee3d67b8fb1af07
Dnyanesh U. Chaudhari <dnyaneshchaudhari05@gmail.com>
2026-04-12 20:40:59 +0530

    migrated the DB from in-memery to MongoDB

---
+++ models.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+const organizationSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    members:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }]
+});
+
+const boardsSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    orgId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization',
+    }
+});
+
+const issueSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['To Do', 'In Progress', 'Done'],
+        default: 'To Do',
+    },
+    boardId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Board',
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+const Organization = mongoose.model("Organization", organizationSchema);
+const Board = mongoose.model("Board", boardsSchema);
+const Issue = mongoose.model("Issue", issueSchema);
+
+export { User, Organization, Board, Issue };
